8d00ba3ea3ada50ca855671f38291ef9f7f43385
Rabbi Khan <Krabby2244@gmail.com>
2025-06-21 00:24:23 +0600

    PaymentSettingModel.js added

---
+++ src/models/PaymentSettingModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const DataSchema = new mongoose.Schema(    //SSL Payment 
+{
+     store_id: {type: String, required: true},
+     store_password: {type: String, required: true},
+     currency: {type: String, required: true},
+     success_url: {type: String, required: true},
+     fail_url: {type: String, required: true},
+     cancel_url: {type: String, required: true},
+     ipn_url: {type: String, required: true},
+     init_url: {type: String, required: true},
+     description: {type: String, required: true},
+     
+},
+{
+    timestamps: true,
+    versionKey: false,
+}
+);
+
+const PaymentSettingModel = mongoose.model("paymentsettings", DataSchema);
+
+export default PaymentSettingModel
