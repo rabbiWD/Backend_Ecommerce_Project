1897666ee719c2f78ebfd892f2f6fd06569d02e3
Rabbi Khan <Krabby2244@gmail.com>
2025-06-21 13:04:06 +0600

    InvoiceProductModel.js updated

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { ProductBrandList } from "../src/controllers/ProductController.js";
 
 
 const router = express.Router();
 
-
+router.get('/productBrandList', ProductBrandList)
 
 export default router;
---
+++ src/services/ProductService.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+import BrandModel from "../models/BrandModel.js"
+
+export const BrandListService = async ()=>{
+    try {
+        const data = await BrandModel
+    } catch (error) {
+        
+    }
+}
