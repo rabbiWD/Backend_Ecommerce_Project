1a6285397d31484bf56616e4be6b3e79937ade0d
Rabbi Khan <Krabby2244@gmail.com>
2025-06-21 18:05:41 +0600

    ProdutCategoryList api added

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { ProductBrandList } from "../src/controllers/ProductController.js";
+import { ProductBrandList, ProductCategoryList } from "../src/controllers/ProductController.js";
 
 
 const router = express.Router();
 
 router.get('/productBrandList', ProductBrandList)
+router.get('/productCategoryList', ProductCategoryList)
 
 export default router;
