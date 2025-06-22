3d0bd8778bbde8197f04c8882041b80d9e6dca70
Rabbi Khan <Krabby2244@gmail.com>
2025-06-22 23:26:45 +0600

    some api added

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { ProductBrandList, ProductCategoryList } from "../src/controllers/ProductController.js";
+import { ProductBrandList, ProductCategoryList, ProductDetails, ProductListByBrand, ProductListByCategory, ProductListByKeyword, ProductListByRemark, ProductListBySmilier, ProductSliderList } from "../src/controllers/ProductController.js";
 
 
 const router = express.Router();
 
-router.get('/productBrandList', ProductBrandList)
-router.get('/productCategoryList', ProductCategoryList)
+router.get('/ProductBrandList', ProductBrandList);
+router.get('/ProductCategoryList', ProductCategoryList);
+router.get('/ProductSliderList', ProductSliderList);
+router.get('/ProductListByBrand/:brandID', ProductListByBrand);
+router.get('/ProductListByCategory/:CategoryID', ProductListByCategory);
+
+router.get('/ProductListBySmilier/:CategoryID', ProductListBySmilier);
+router.get('/ProductListByKeyword/:KeywordID', ProductListByKeyword);
+router.get('/ProductListByRemark/:RemarkID', ProductListByRemark);
+router.get('/ProductDetails/:ProductID', ProductDetails);
 
 export default router;
