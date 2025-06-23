df1023e0040ef099a39f55e5ccf22d11618495b9
Rabbi Khan <Krabby2244@gmail.com>
2025-06-24 01:01:24 +0600

    some service list added

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,6 +3,7 @@ import BrandModel from "../models/BrandModel.js"
 import CategoryModel from "../models/CategoryModel.js";
 import ProductSliderModel from "../models/ProductSliderModel.js";
 import ProductModel from "../models/ProductModel.js";
+import ReviewModel from "../models/ReviewModel.js";
 
 const ObjectId = mongoose.Types.ObjectId;
 
@@ -299,3 +300,73 @@ export const DetailsService = async(req)=>{
          return {status: "fail", data: err}.toString();
     }
 }
+
+export const ReviewListService = async (req) => {
+    try {
+        const ProductID = new ObjectId(req.params.ProductID);
+
+        const data = await ReviewModel.aggregate([
+            { $match: { productID: ProductID } },
+            { $lookup: { from: 'profiles', localField: 'userID', foreignField: 'userID', as: 'profile' } },
+            { $unwind: '$profile' },
+            { $project: { des: 1, rating: 1, 'profile.cus_name': 1 } },
+        ]);
+
+        return { status: 'success', data };
+    } catch (e) {
+        return { status: 'fail', data: e.toString() };
+    }
+};
+
+export const CreateReviewService = async (req) => {
+    try {
+        const user_id = req.headers.user_id;
+        const reqBody = req.body;
+
+        const data = await ReviewModel.create({
+            productID: reqBody.productID,
+            userID: user_id,
+            des: reqBody.des,
+            rating: reqBody.rating,
+        });
+
+        return {status: "success", data: data};
+    } catch (err) {
+        //  return {status: "fail", data: err}.toString();
+         return { status: "fail", data: err.toString() };
+    }
+};
+
+export const ListByFilterService = async (req) => {
+    try {
+        const matchConditions = {};
+        if (req.body.categoryID) {
+            matchConditions.categoryID = new ObjectId(req.body.categoryID);
+        }
+        if (req.body.brandID) {
+            matchConditions.brandID = new ObjectId(req.body.brandID);
+        }
+
+        const priceMin = parseInt(req.body.priceMin);
+        const priceMax = parseInt(req.body.priceMax);
+
+        const priceMatch = {};
+        if (!isNaN(priceMin)) priceMatch.numericPrice = { $gte: priceMin };
+        if (!isNaN(priceMax)) priceMatch.numericPrice = { ...(priceMatch.numericPrice || {}), $lte: priceMax };
+
+        const data = await ProductModel.aggregate([
+            { $match: matchConditions },
+            { $addFields: { numericPrice: { $toInt: '$price' } } },
+            { $match: priceMatch },
+            { $lookup: { from: 'brands', localField: 'brandID', foreignField: '_id', as: 'brand' } },
+            { $lookup: { from: 'categories', localField: 'categoryID', foreignField: '_id', as: 'category' } },
+            { $unwind: '$brand' },
+            { $unwind: '$category' },
+            { $project: { 'brand._id': 0, 'category._id': 0, categoryID: 0, brandID: 0 } },
+        ]);
+
+        return { status: 'success', data };
+    } catch (e) {
+        return { status: 'fail', data: e.toString() };
+    }
+};
